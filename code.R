1bb2396a29f0c249098f7aaa29484c84bc08929a
Khaled Hamady <khaledhamady08@gmail.com>
2026-08-29 17:39:54 +0200

    Add files via upload

---
+++ code.R
@@ -0,0 +1,16 @@
+getwd()
+
+install.packages("tidyverse")
+install.packages("psych")
+
+library(readr)
+library(psych)
+library(dplyr)
+library(ggplot2)
+
+list.files()
+
+df <- read_csv("SeoulBikeData.csv",
+               locale= locale(encoding = "Windows-1252"))
+head(df)
+View(df)
