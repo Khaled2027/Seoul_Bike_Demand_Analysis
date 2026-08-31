0d64f0b9fbf9d4262da5c48d2b8429cab4c68c01
Khaled Hamady <khaledhamady08@gmail.com>
2026-08-31 22:58:25 +0200

    Add files via upload

--- code.R
+++ code.R
@@ -9,7 +9,6 @@ library(psych)
 library(dplyr)
 library(ggplot2)
 
-list.files()
 
 df <- read_csv("SeoulBikeData.csv",
                locale= locale(encoding = "Windows-1252"))
@@ -21,3 +20,10 @@ str(df)
 sum(is.na(df))
 # Returns the number of duplicate rows
 sum(duplicated(df))
+
+# Converts the datatype of 'Rented Bike Count' column into numeric type
+df <-df %>%
+  mutate(`Rented Bike Count` = as.numeric(`Rented Bike Count`))
+# Displays a histogram based on 'Rented Bike Count' column
+hist(df$`Rented Bike Count`,main=paste("Histogram of",
+      "Rented Bike Count"),xlab='Number of rented bikes',col="lightblue") 
