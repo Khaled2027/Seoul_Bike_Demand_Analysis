590f95dbddb082b897a4c6253bfd6ec21219fc25
Khaled Hamady <khaledhamady08@gmail.com>
2026-09-01 17:54:06 +0200

    Add files via upload

--- code.R
+++ code.R
@@ -24,6 +24,16 @@ sum(duplicated(df))
 # Converts the datatype of 'Rented Bike Count' column into numeric type
 df <-df %>%
   mutate(`Rented Bike Count` = as.numeric(`Rented Bike Count`))
+
+# Returns a dataframe with Seasons grouped and 
+# aggregated by the mean bike demand per season
+df_groupby_season <- df %>%
+  group_by(Seasons) %>%
+  summarise(avgDemand=sum(`Rented Bike Count`)/length(Date))
+df_groupby_sesson
+
 # Displays a histogram based on 'Rented Bike Count' column
 hist(df$`Rented Bike Count`,main=paste("Histogram of",
       "Rented Bike Count"),xlab='Number of rented bikes',col="lightblue") 
+
+
