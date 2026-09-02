b3e9ccc7b3c2b5d64f007da3b34762b798a2d548
Khaled Hamady <khaledhamady08@gmail.com>
2026-09-02 20:09:44 +0200

    Add files via upload

--- code.R
+++ code.R
@@ -24,6 +24,22 @@ sum(is.na(df))
 # Returns the number of duplicate rows
 sum(duplicated(df))
 
+length_of_functioning_day <- length(df$`Functioning Day`)
+
+percentage <- (table(df$`Functioning Day`)/length_of_functioning_day)*100
+# Rounds to 2 decimal places
+percentage <- round(percentage,2)
+percentage
+
+df_groupby_functioning_day <- df %>%
+  select(`Rented Bike Count`,`Functioning Day`) %>%
+  filter(`Functioning Day`=='No')
+df_groupby_functioning_day
+
+# keeps rows where`Functioning Day' column is equal to 'Yes'
+df <- df %>%
+  filter(`Functioning Day`=='Yes')
+
 # Converts the datatype of 'Rented Bike Count' column into numeric type
 df <-df %>%
   mutate(`Rented Bike Count` = as.numeric(`Rented Bike Count`))
